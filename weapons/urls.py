08f810f80d200c4eef2d0247bae775596890c7b6
LouisCE <louiscowell190@yahoo.co.uk>
2025-11-20 02:42:56 +0000

    Add weapon detail view and template

--- weapons/urls.py
+++ weapons/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.weapon_list, name='weapon_list'),
+    path('<int:pk>/', views.weapon_detail, name='weapon_detail'),
 ]
--- weapons/views.py
+++ weapons/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Weapon
 
 # Create your views here.
@@ -7,3 +7,8 @@ from .models import Weapon
 def weapon_list(request):
     weapons = Weapon.objects.all()
     return render(request, 'weapons/weapon_list.html', {'weapons': weapons})
+
+
+def weapon_detail(request, pk):
+    weapon = get_object_or_404(Weapon, pk=pk)
+    return render(request, 'weapons/weapon_detail.html', {'weapon': weapon})
