c08dbed3362a48dc0b4c25a3ca6fb677d78f5fe3
LouisCE <louiscowell190@yahoo.co.uk>
2025-11-20 15:04:10 +0000

    Fix weapon_detail URL pattern for Heroku deployment

--- morphgun/urls.py
+++ morphgun/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.weapon_list, name='weapon_list'),
     path('create/', views.create_weapon, name='create_weapon'),
-    path('<int:pk>/', views.weapon_detail, name='weapon_detail'),
+    path('weapon/<int:pk>/', views.weapon_detail, name='weapon_detail'),
 ]
