b7a78d73481086d3eb1a9f1373ab6afd03e2ab7c
LouisCE <louiscowell190@yahoo.co.uk>
2025-11-23 17:19:30 +0000

    Add update and delete weapon views

--- morphgun/views.py
+++ morphgun/views.py
@@ -33,3 +33,27 @@ def create_weapon(request):
     else:
         form = WeaponForm()
     return render(request, 'morphgun/create_weapon.html', {'form': form})
+
+
+# Update view
+@user_passes_test(staff_check)
+def update_weapon(request, pk):
+    weapon = get_object_or_404(Weapon, pk=pk)
+    if request.method == 'POST':
+        form = WeaponForm(request.POST, request.FILES, instance=weapon)
+        if form.is_valid():
+            form.save()
+            return redirect('weapon_detail', pk=weapon.pk)
+    else:
+        form = WeaponForm(instance=weapon)
+    return render(request, 'morphgun/update_weapon.html', {'form': form, 'weapon': weapon})
+
+
+# Delete view
+@user_passes_test(staff_check)
+def delete_weapon(request, pk):
+    weapon = get_object_or_404(Weapon, pk=pk)
+    if request.method == 'POST':
+        weapon.delete()
+        return redirect('weapon_list')
+    return render(request, 'morphgun/delete_weapon.html', {'weapon': weapon})
