f7daddf32b7e99d662ee291cc96209e25ba1149d
LouisCE <louiscowell190@yahoo.co.uk>
2025-11-23 17:12:48 +0000

    Add HomePage model with hero image

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class HomePage(models.Model):
+    hero_image = CloudinaryField('image', default='placeholder')
+    hero_title = models.CharField(max_length=200, default="Adventure Awaits in Haven City")
+    hero_text = models.TextField(default="Explore Morph Gun mods, Characters, Collectibles and more!")
