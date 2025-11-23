b31a7fe3c987ce651a71087aa21b5bb615635e07
LouisCE <louiscowell190@yahoo.co.uk>
2025-11-23 17:16:01 +0000

    Add update and delete weapon URLs

--- morphgun/urls.py
+++ morphgun/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.weapon_list, name='weapon_list'),
     path('create/', views.create_weapon, name='create_weapon'),
     path('weapon/<int:pk>/', views.weapon_detail, name='weapon_detail'),
+    path('weapon/<int:pk>/edit/', views.update_weapon, name='update_weapon'),
+    path('weapon/<int:pk>/delete/', views.delete_weapon, name='delete_weapon'),
 ]
