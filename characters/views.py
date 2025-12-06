210d2fb6cd7a4f27e68899ed0146a1ca98ab3b8d
LouisCE <louiscowell190@yahoo.co.uk>
2025-12-06 20:04:03 +0000

    Restrict character CRUD to staff users

--- characters/views.py
+++ characters/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Character
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 class CharacterList(ListView):
     model = Character
@@ -12,19 +14,30 @@ class CharacterDetail(DetailView):
     model = Character
     template_name = "characters/character_detail.html"
 
-class CharacterCreate(CreateView):
+class CharacterCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Character
     fields = "__all__"
     template_name = "characters/character_form.html"
     success_url = reverse_lazy("character_list")
 
-class CharacterUpdate(UpdateView):
+    def test_func(self):
+        return self.request.user.is_staff
+
+
+class CharacterUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Character
     fields = "__all__"
     template_name = "characters/character_form.html"
     success_url = reverse_lazy("character_list")
 
-class CharacterDelete(DeleteView):
+    def test_func(self):
+        return self.request.user.is_staff
+
+
+class CharacterDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Character
     template_name = "characters/character_confirm_delete.html"
     success_url = reverse_lazy("character_list")
+
+    def test_func(self):
+        return self.request.user.is_staff
