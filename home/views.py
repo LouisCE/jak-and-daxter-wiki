17da4eca426225f89e6f74449428c07d028c54bd
LouisCE <louiscowell190@yahoo.co.uk>
2025-12-06 20:03:04 +0000

    Add register view and form handling

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 def index(request):
     return render(request, 'home/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'home/register.html', {'form': form})
