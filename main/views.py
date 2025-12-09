ee0fccd48551fba505d4b3164436eceb8782aca6
LouisCE <louiscowell190@yahoo.co.uk>
2025-12-09 20:32:28 +0000

    Add custom error handler views

---
+++ main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+
+def handler400(request, exception):
+    """ Error Handler 400 - Bad Request """
+    return render(request, "errors/400.html", status=400)
+
+
+def handler403(request, exception):
+    """ Error Handler 403 - Forbidden """
+    return render(request, "errors/403.html", status=403)
+
+
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
+
+
+def handler429(request, timeout=0):
+    """ Error Handler 429 - Too Many Requests """
+    return render(request, "errors/429.html", status=429)
+
+
+def handler500(request):
+    """ Error Handler 500 - Internal Server Error """
+    return render(request, "errors/500.html", status=500)
