383b94d2f4a179020e0bac7b5e9fcdc39a52d548
LouisCE <louiscowell190@yahoo.co.uk>
2025-12-12 20:00:25 +0000

    Add Collectable model and initial migration

--- collectables/models.py
+++ collectables/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Collectable(models.Model):
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    image = CloudinaryField("image", default="placeholder", blank=True)
+
+    # Custom ordering
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.name
