7bc62a3278fbabbfca080d08f1ee3930255afeb3
LouisCE <louiscowell190@yahoo.co.uk>
2025-12-12 21:07:22 +0000

    Add Collectable model form

---
+++ collectables/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Collectable
+
+
+class CollectableForm(forms.ModelForm):
+    class Meta:
+        model = Collectable
+        fields = ['name', 'description', 'image']
