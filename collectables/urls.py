ab56298b7df0bb5e97413c827059a4c49b241ed8
LouisCE <louiscowell190@yahoo.co.uk>
2025-12-12 21:23:47 +0000

    Add collectables URL routes

---
+++ collectables/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.collectable_list, name='collectable_list'),
+    path('create/', views.collectable_create, name='collectable_create'),
+    path('<int:pk>/update/', views.collectable_update, name='collectable_update'),
+    path('<int:pk>/delete/', views.collectable_delete, name='collectable_delete'),
+]
