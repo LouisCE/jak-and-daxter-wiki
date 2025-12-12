4afc0e8cc448ea7f8885f5d7cad5ca3b5ddee03f
LouisCE <louiscowell190@yahoo.co.uk>
2025-12-12 21:25:57 +0000

    Add collectables CRUD views

--- collectables/views.py
+++ collectables/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import user_passes_test
+from .models import Collectable
+from .forms import CollectableForm
 
-# Create your views here.
+def staff_check(user):
+    return user.is_staff
+
+def collectable_list(request):
+    items = Collectable.objects.order_by('order', 'name')
+    return render(request, 'collectables/collectable_list.html', {'items': items})
+
+@user_passes_test(staff_check)
+def collectable_create(request):
+    if request.method == 'POST':
+        form = CollectableForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('collectable_list')
+    else:
+        form = CollectableForm()
+    return render(request, 'collectables/collectable_form.html', {'form': form})
+
+@user_passes_test(staff_check)
+def collectable_update(request, pk):
+    item = get_object_or_404(Collectable, pk=pk)
+    if request.method == 'POST':
+        form = CollectableForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('collectable_list')
+    else:
+        form = CollectableForm(instance=item)
+    return render(request, 'collectables/collectable_form.html', {'form': form, 'item': item})
+
+@user_passes_test(staff_check)
+def collectable_delete(request, pk):
+    item = get_object_or_404(Collectable, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('collectable_list')
+    return render(request, 'collectables/collectable_confirm_delete.html', {'item': item})
