994d2daa9e1703106e5c3b810f3505cc4ed48472
LouisCE <louiscowell190@yahoo.co.uk>
2025-12-17 07:21:30 +0000

    Add WeaponRating model form

--- morphgun/forms.py
+++ morphgun/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Weapon, Colour
+from .models import WeaponRating
 
 
 class ColourForm(forms.ModelForm):
@@ -12,3 +13,14 @@ class WeaponForm(forms.ModelForm):
     class Meta:
         model = Weapon
         fields = ['name', 'colour', 'description', 'image']
+
+class WeaponRatingForm(forms.ModelForm):
+    class Meta:
+        model = WeaponRating
+        fields = ['score']
+        widgets = {
+            'score': forms.Select(
+                choices=[(i, i) for i in range(1, 11)],
+                attrs={'class': 'form-select'}
+            )
+        }
