d31447277fc90ada135a8631339724632f181a42
LouisCE <louiscowell190@yahoo.co.uk>
2026-01-10 06:36:47 +0000

    Tighten Collectable model field constraints

--- collectables/models.py
+++ collectables/models.py
@@ -4,9 +4,9 @@ from cloudinary.models import CloudinaryField
 
 class Collectable(models.Model):
 
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    image = CloudinaryField("image", default="placeholder", blank=True)
+    name = models.CharField(max_length=20, blank=False)
+    description = models.TextField(blank=False)
+    image = CloudinaryField("image", default="placeholder", blank=False)
 
     # Custom ordering
     order = models.PositiveIntegerField(default=0)
