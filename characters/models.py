623409725409b083af23526b6d5f08611807a6d3
LouisCE <louiscowell190@yahoo.co.uk>
2026-01-10 06:36:04 +0000

    Tighten Character model field constraints

--- characters/models.py
+++ characters/models.py
@@ -4,22 +4,22 @@ from cloudinary.models import CloudinaryField
 
 class Character(models.Model):
     """Character model."""
-    name = models.CharField(max_length=100)
-    quote = models.CharField(max_length=255, blank=True)
-    image = CloudinaryField("image", default="placeholder", blank=True)
+    name = models.CharField(max_length=20)
+    quote = models.CharField(max_length=250, blank=False)
+    image = CloudinaryField("image", default="placeholder", blank=False)
 
     # Stats
-    sex = models.CharField(max_length=20, blank=True)
-    age = models.CharField(max_length=20, blank=True)
-    skin = models.CharField(max_length=50, blank=True)
-    hair = models.CharField(max_length=50, blank=True)
-    eyes = models.CharField(max_length=50, blank=True)
-    height = models.CharField(max_length=50, blank=True)
-    weight = models.CharField(max_length=50, blank=True)
-    occupation = models.CharField(max_length=100, blank=True)
+    sex = models.CharField(max_length=20, blank=False)
+    age = models.CharField(max_length=20, blank=False)
+    skin = models.CharField(max_length=20, blank=False)
+    hair = models.CharField(max_length=20, blank=False)
+    eyes = models.CharField(max_length=20, blank=False)
+    height = models.CharField(max_length=20, blank=False)
+    weight = models.CharField(max_length=20, blank=False)
+    occupation = models.CharField(max_length=20, blank=False)
 
-    appearance = models.TextField(blank=True)
-    personality = models.TextField(blank=True)
+    appearance = models.TextField(blank=False)
+    personality = models.TextField(blank=False)
 
     # Custom ordering
     order = models.PositiveIntegerField(default=0)
