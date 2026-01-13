0a775c9bb4ce2e4010bb54225f4741f02bdd17d4
LouisCE <louiscowell190@yahoo.co.uk>
2026-01-13 16:23:53 +0000

    Relax Character model field constraints

--- characters/models.py
+++ characters/models.py
@@ -4,19 +4,19 @@ from cloudinary.models import CloudinaryField
 
 class Character(models.Model):
     """Character model."""
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     quote = models.CharField(max_length=250, blank=False)
-    image = CloudinaryField("image", default="placeholder", blank=False)
+    image = CloudinaryField("image", default="placeholder")
 
-    # Stats
-    sex = models.CharField(max_length=20, blank=False)
-    age = models.CharField(max_length=20, blank=False)
-    skin = models.CharField(max_length=20, blank=False)
-    hair = models.CharField(max_length=20, blank=False)
-    eyes = models.CharField(max_length=20, blank=False)
-    height = models.CharField(max_length=20, blank=False)
-    weight = models.CharField(max_length=20, blank=False)
-    occupation = models.CharField(max_length=20, blank=False)
+    # Character bios
+    sex = models.CharField(max_length=50, blank=False)
+    age = models.CharField(max_length=50, blank=False)
+    skin = models.CharField(max_length=50, blank=False)
+    hair = models.CharField(max_length=50, blank=False)
+    eyes = models.CharField(max_length=50, blank=False)
+    height = models.CharField(max_length=50, blank=False)
+    weight = models.CharField(max_length=50, blank=False)
+    occupation = models.CharField(max_length=50, blank=False)
 
     appearance = models.TextField(blank=False)
     personality = models.TextField(blank=False)
