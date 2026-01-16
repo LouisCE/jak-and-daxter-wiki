3108bb6e4da42ed9e5b8673bc1fe6f0e5ca8ba77
LouisCE <louiscowell190@yahoo.co.uk>
2026-01-16 10:58:53 +0000

    Refactor Python for PEP8 compliance

--- home/models.py
+++ home/models.py
@@ -1,8 +1,19 @@
-from django.db import models
 from cloudinary.models import CloudinaryField
+from django.db import models
 
 
 class HomePage(models.Model):
-    hero_image = CloudinaryField('image', default='placeholder')
-    hero_title = models.CharField(max_length=200, default="Adventure Awaits in Haven City")
-    hero_text = models.TextField(default="Explore Morph Gun mods, Characters, Collectibles and more!")
+    hero_image = CloudinaryField(
+        "image",
+        default="placeholder",
+    )
+    hero_title = models.CharField(
+        max_length=200,
+        default="Adventure Awaits in Haven City",
+    )
+    hero_text = models.TextField(
+        default=(
+            "Explore Morph Gun mods, Characters, "
+            "Collectibles and more!"
+        ),
+    )
--- main/urls.py
+++ main/urls.py
@@ -1,39 +1,70 @@
 """
 URL configuration for main project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information
+see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    1. Add an import: from my_app import views
+    2. Add a URL to urlpatterns: path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    1. Add an import: from other_app.views import Home
+    2. Add a URL to urlpatterns: path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function:
+       from django.urls import include, path
+    2. Add a URL to urlpatterns:
+       path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
 from home import views
-from .views import handler400, handler403, handler404, handler429, handler500
+from .views import (
+    handler400,
+    handler403,
+    handler404,
+    handler429,
+    handler500,
+)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),        # Allauth
-    path('summernote/', include('django_summernote.urls')),  # Summernote
-    path('morphgun/', include('morphgun.urls')),       # Morphgun
-    path('characters/', include('characters.urls')),  # Characters
-    path('collectables/', include('collectables.urls')),  # Collectables
-    path('', include('home.urls')),                    # Home app URLs
+    path("admin/", admin.site.urls),
+    path(
+        "accounts/",
+        include("allauth.urls"),
+    ),  # Allauth
+    path(
+        "summernote/",
+        include("django_summernote.urls"),
+    ),  # Summernote
+    path(
+        "morphgun/",
+        include("morphgun.urls"),
+    ),  # Morphgun
+    path(
+        "characters/",
+        include("characters.urls"),
+    ),  # Characters
+    path(
+        "collectables/",
+        include("collectables.urls"),
+    ),  # Collectables
+    path(
+        "",
+        include("home.urls"),
+    ),  # Home app URLs
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
 
 
 handler400 = "main.views.handler400"
